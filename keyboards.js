edf63b982778064a1c48eb35488df994e696b057
Werwllleh <allvxm4@gmail.com>
2022-09-16 18:14:16 +0300

    1

--- keyboards.js
+++ keyboards.js
@@ -20,7 +20,7 @@ module.exports = {
 					{
 						text: "Регистрация",
 						web_app: {
-							url: "https://forty-months-check-85-234-6-154.loca.lt/form.html",
+							url: "https://eager-beers-nail-85-234-6-154.loca.lt/form.html",
 						},
 					},
 				],
