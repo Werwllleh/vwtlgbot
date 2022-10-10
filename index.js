9535de4a5d6dbb5644e27926fb1d9da4683faa82
Werwllleh <allvxm4@gmail.com>
2022-10-10 14:16:40 +0300

    8

--- index.js
+++ index.js
@@ -22,6 +22,11 @@ app.use(express.static('static'))
 // app.use(fileUpload({}))
 // app.use('/api', router)
 
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
